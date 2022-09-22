1acb690d0c3f93255be947c2846436ef48879c4c
Tony Diethelm <tonydiethelm@gmail.com>
2022-09-21 19:50:43 -0700

    Attempt number 1!

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,8 @@
 console.log("Pikachu Everywhere - Content Script is Running");
+
+
+let images = document.getElementsByTagName('img');
+console.log('these are all the imags on our page: ', images);
+for(let i = 0; i < images.length; i++){
+      images[index].src = './pikachu.png';
+    }
