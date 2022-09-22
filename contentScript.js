0d5d09dfc345cb88287124ccf5db4a2729a1a061
Tony Diethelm <tonydiethelm@gmail.com>
2022-09-21 21:41:57 -0700

    Done! Working! Demo model with demo times.

--- contentScript.js
+++ contentScript.js
@@ -1,58 +1,26 @@
 console.log("Stretch Break! - Content Script is Running");
 
 /*
-We want to add a giant DIV to the screen on all pages.
-    We'll need to style that div to be black and rounded edges and have some text say "Go stretch!"
-We need to know where to put it. document? window? 
-    Window.document, but iframes are their own documents inside the window. We want to cover everything. 
-    Let's go to document for now. 
+We want to enforce stretch breaks. 
+We are going to change all the text to "go stretch!" and change the images to a funny stretching gif.
 */
 
-// make a div with styling.
-    //document.createElement('div
-    //document.getElementById('stretchBreak').innerHTML = 'Stretch Break!';
-// append it to the document. 
-
-// let screenBlock = document.createElement('div');
-// screenBlock.innerHTML = 'Stretch Break!';
-// screenBlock.id = 'StretchBreak';
-// //append to document
-// document.body.appendChild(screenBlock);         //this appends the div, but it's on the END of the document. Not working. 
 
 
-
-// We might be able to just change the body background color? Nope. 
-//document.body.style.backgroundColor = black;    //nope
-//document.body.style.cssText = 'background-color: black';    //that works, but only changes background, and you can still see stuff. 
-
-
-
-
-
-
-// change the styling on document.body to be black background, black text, etc. 
-// and change it back when we're done. 
-
-
-
-
-
-
-
-
-
-
-let images = document.getElementsByTagName('img');
-for(let eachImage of images){
-      eachImage.src = 'https://i.imgur.com/5ocRuPi.gif';
+function stretchBreak (){
+    //change all the images
+    let images = document.getElementsByTagName('img');
+    for(let eachImage of images){
+        eachImage.src = 'https://i.imgur.com/5ocRuPi.gif';
     }
+    //change all the text.
+    let texts = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, pation, span, a');
+    for(let eachOne of texts){
+        eachOne.innerHTML = "Go Stretch!";
+    }
+};
 
-
-
-
-//New Idea! Make all the text "go stretch!"
-
-let texts = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, pation, span, a');
-for(let eachOne of texts){
-    eachOne.innerHTML = "Go Stretch!";
-}
+const timeInSeconds = 10
+const timeInMilliSeconds = timeInSeconds * 1000;
+setTimeout(stretchBreak, timeInMilliSeconds);
+setTimeout(function(){window.location.reload();}, timeInMilliSeconds + 5000);
